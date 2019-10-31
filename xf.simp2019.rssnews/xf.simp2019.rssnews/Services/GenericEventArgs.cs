beab4eaa9460a318de5677ce538eddabcf1f6aac
Israel Calderón de la Cruz <icalderondev@outlook.com>
2019-10-31 08:22:03 -0600

    Definienco estructura

---
+++ xf.simp2019.rssnews/xf.simp2019.rssnews/Services/GenericEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace xf.simp2019.rssnews.Services
+{
+    public class GenericEventArgs<T> : EventArgs
+    {
+        public IList<T> Results { get; private set; }
+        public GenericEventArgs(IList<T> results)
+        {
+            Results = results;
+        }
+    }
+}
---
+++ xf.simp2019.rssnews/xf.simp2019.rssnews/ViewModels/NotificationEnabledObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace xf.simp2019.rssnews.ViewModels
+{
+    public class NotificationEnabledObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool Set<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Object.Equals(storage, value))
+                return false;
+            storage = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+    }
+}
